4b7647fa61abbdcf51bd226413011effe6907d61
Lyrn Mukami <mbotelyrn@gmail.com>
2024-01-04 09:52:16 +0300

    add staff template pages, urls, and views

--- rental_website/apps/employee/urls.py
+++ rental_website/apps/employee/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name="employee"
 urlpatterns = [
     path('dashboard', views.index, name="dashboard"),
+    path('listings', views.viewProperty, name="listings"),
+    path('newProperty', views.addProperty, name="newProperty"),
+    path('editProperty', views.editProperty, name="editProperty"),
+    path('deleteProperty', views.deleteProperty, name="deleteProperty"),
 ]
--- rental_website/apps/employee/views.py
+++ rental_website/apps/employee/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'employee/dashboard.html')
+
+def viewProperty(request):
+     return render(request, 'employee/viewProperty.html')
+
+def addProperty(request):
+    return render(request, 'employee/addProperty.html')
+
+def editProperty(request):
+     return render(request, 'employee/editProperty.html')
+     
+def deleteProperty(request):
+     return render(request, 'employee/viewProperty.html')
