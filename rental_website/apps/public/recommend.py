c7dd27c3acd11ca505b3f540dd4dea45312822e9
Lyrn Mukami <mbotelyrn@gmail.com>
2024-01-10 16:50:56 +0300

    critiquing module

--- rental_website/apps/public/recommend.py
+++ rental_website/apps/public/recommend.py
@@ -1,5 +1,6 @@
 from joblib import load
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 model = load('rental_website/SavedModel/model.joblib')
@@ -7,7 +8,53 @@ model = load('rental_website/SavedModel/model.joblib')
 class Recommender():
     def recommend(data):
         #transforming dictionary to a dataframe
-        data= pd.DataFrame.from_dict(data)
+        data = pd.DataFrame.from_dict(data)
+        data = data.drop(data[data['choice'] == 'no'].index)
+        data = data.drop(['choice'], axis=1)
+        features_df = pd.read_csv('rental_website/SavedModel/features.csv')
+        features_df = features_df.drop(['Unnamed: 0'], axis=1)
+        encoded_df = pd.read_csv('rental_website/SavedModel/encoded_df.csv')
+        encoded_df =encoded_df.drop(['Unnamed: 0'], axis=1)
+        clustered_df = pd.read_csv('rental_website/SavedModel/clustered.csv')
+        clustered_df = clustered_df.drop(['Unnamed: 0'], axis=1)
+        #encode the location
+        label_encoder = LabelEncoder()
+        label_encoder.fit(features_df['location'])
+        data['location'] = label_encoder.transform(data['location'])
+
+        #normalizing the data
+        scaler = MinMaxScaler()
+        scaler.fit(encoded_df)
+        data[['location','bedrooms','bathrooms','price']] = scaler.transform(data)
+
+        #Predict the cluster
+        pred = model.predict(data)
+
+        data['cluster'] = pred
+        pred = pred[0]
+        #Retrieve the listings from the same cluster
+        if pred == 0:
+            property_df = clustered_df[clustered_df['cluster'] == 0]
+        elif pred == 1:
+            property_df = clustered_df[clustered_df['cluster'] == 1]
+        else:
+            property_df = clustered_df[clustered_df['cluster'] == 2]
+
+        #Perform cosine similarity
+        cosine_similarity_score = cosine_similarity(property_df, data)
+        cosine_similarity_score = np.mean(cosine_similarity_score, axis=1)
+        property_df['cos_score'] = cosine_similarity_score
+        #Sorting the data by cos_score descending
+        sorted_df = property_df.sort_values(['cos_score'], ascending=False)
+        #Replacing normalized values with actual values
+        sorted_df.loc[:, ['location', 'bedrooms', 'bathrooms','price']] = features_df[['location', 'bedrooms', 'bathrooms','price']]
+        sorted_df = sorted_df.iloc[ :10]
+        final_df = sorted_df.sort_values(['price'])
+        return final_df
+
+    def critique(data):
+        #transforming dictionary to a dataframe
+        data = pd.DataFrame.from_dict(data)
         features_df = pd.read_csv('rental_website/SavedModel/features.csv')
         features_df = features_df.drop(['Unnamed: 0'], axis=1)
         encoded_df = pd.read_csv('rental_website/SavedModel/encoded_df.csv')
@@ -40,8 +87,11 @@ class Recommender():
         cosine_similarity_score = cosine_similarity(property_df, data)
         property_df['cos_score'] = cosine_similarity_score
         #Sorting the data by cos_score descending
-        sorted_df = property_df.sort_values(['cos_score'], ascending=False)
+        sorted_df = property_df.sort_values(['cos_score' ,'price'], ascending=[False,True])
         #Replacing normalized values with actual values
         sorted_df.loc[:, ['location', 'bedrooms', 'bathrooms','price']] = features_df[['location', 'bedrooms', 'bathrooms','price']]
-        sorted_df = sorted_df.iloc[ :10 ,:-2]
-        return sorted_df
+        # sorted_df = sorted_df.sample(n=3, weights=sorted_df['cos_score'])
+        sorted_df = sorted_df.iloc[:3,]
+        final_df = sorted_df.sort_values(['price'])
+        return final_df
+        
--- rental_website/apps/public/views.py
+++ rental_website/apps/public/views.py
@@ -10,21 +10,26 @@ def index(request):
 def preferences(request):
     return render(request, 'preferences.html')
 def listings(request):
-    location = request.GET['location']
-    bedrooms = request.GET['bedrooms']
-    bathrooms = request.GET['bathrooms']
-    price = request.GET['price']
-    property = {'location':[location], 'bedrooms':[bathrooms], 'bathrooms':[bathrooms], 'price':[price]}
+    choice = request.GET.getlist('choice')
+    location = request.GET.getlist('location')
+    bedrooms = request.GET.getlist('bedrooms')
+    bathrooms = request.GET.getlist('bathrooms')
+    price = request.GET.getlist('price')
+    property = {'location':location, 'bedrooms':bathrooms, 'bathrooms':bathrooms, 'price':price, 'choice':choice}
+    print(property)
     recommendations = Recommender.recommend(property)
     recommendations = recommendations.reset_index().to_json(orient ='records') 
     data = []
     data = json.loads(recommendations)
-    
-    # return JsonResponse(recommendations)
-    return render(request, 'listings.html',{'rec':data})
+    return render(request, 'listings.html', {'rec':data})
 def critique(request):
-    if request.method == 'POST':
-        choice = request.GET['choice']
-        return choice
-    
-    return render(request, 'critiquing.html')
+    location = request.GET['location']
+    bedrooms = request.GET['bedrooms']
+    bathrooms = request.GET['bathrooms']
+    price = request.GET['price']
+    property = {'location':[location], 'bedrooms':[bedrooms], 'bathrooms':[bathrooms], 'price':[price]}
+    recommendations = Recommender.critique(property) 
+    recommendations = recommendations.reset_index().to_json(orient ='records') 
+    data = []
+    data = json.loads(recommendations)            
+    return render(request, 'critiquing.html',{'rec':data})
